d91759077b12d782c3dfa946bd1c14d4e3f1c560
Rawa Jalal <rawa.jalal@gmail.com>
2017-07-13 02:53:14 -0400

    Admin Dashboard

--- Blogcatch/Areas/Admin/Controllers/DashboardController.cs
+++ Blogcatch/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Blogcatch.Models;
+using Blogcatch.ViewModel;
 
 namespace Blogcatch.Areas.Admin.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Admin/Dashboard
         public ActionResult Index()
         {
-            return View();
+            var pages = _context.Pages
+                .ToArray()
+                .OrderBy(p => p.Sorting)
+                .Select(p => new PageViewModel(p))
+                .ToList();
+  
+            return View(pages);
         }
     }
 }
---
+++ Blogcatch/ViewModel/PageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Blogcatch.Areas.Admin.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blogcatch.ViewModel
+{
+    public class PageViewModel
+    {
+        public PageViewModel(Page page)
+        {
+            this.Title = page.Title;
+            this.Slug = page.Slug;
+            this.Body = page.Body;
+            this.Sorting = page.Sorting;
+            this.HasSidebar = page.HasSidebar;
+        }
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [StringLength(50)]
+        public string Slug { get; set; }
+
+        [MinLength(3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+
+        public bool HasSidebar { get; set; }
+    }
+}
