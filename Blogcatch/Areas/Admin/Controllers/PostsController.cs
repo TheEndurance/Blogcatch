67323cc01d5a3c0eccf130a8e9b10e5f1b8b5c15
Rawa Jalal <rawa.jalal@gmail.com>
2017-07-23 19:13:11 -0400

    Add Post

--- Blogcatch/Areas/Admin/Controllers/PostsController.cs
+++ Blogcatch/Areas/Admin/Controllers/PostsController.cs
@@ -1,5 +1,8 @@
-﻿using Blogcatch.Models;
+﻿using Blogcatch.Areas.Admin.Models;
+using Blogcatch.Models;
 using Blogcatch.ViewModel;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -33,7 +36,52 @@ namespace Blogcatch.Areas.Admin.Controllers
         public ActionResult AddPost()
         {
             var postVM = new PostViewModel();
-            return View("PostForm",postVM);
+            return View("PostForm", postVM);
+        }
+
+        public ActionResult AddPost(PostViewModel postVM)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (!ModelState.IsValid)
+            {
+                return View("PostForm", postVM);
+            }
+            var post = new Post();
+
+            string slug = "";
+
+            post.Title = postVM.Title;
+            slug = (string.IsNullOrWhiteSpace(postVM.Slug))
+                ? postVM.Title.Replace(" ", "-")
+                : postVM.Slug.Replace(" ", "-");
+
+            if (_context.Posts.Any(p => p.Title == postVM.Title || p.Slug == slug))
+            {
+                ModelState.AddModelError("", "That title or slug already exists");
+            }
+
+            string excerpt = postVM.Content;
+            int length = 300;
+            if (postVM.Content.Length > length)
+            {
+                int lastSpace = postVM.Content.LastIndexOf(" ", length);
+                excerpt = $"{postVM.Content.Substring(0, (lastSpace > 0) ? lastSpace : length)}" + "...";
+
+            }
+
+
+            post.Slug = slug;
+            post.PostDate = DateTime.Now;
+            post.AllowComments = postVM.AllowComments;
+            post.Content = postVM.Content;
+            post.Excerpt = excerpt;
+            post.AuthorId = userId;
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+            TempData["SM"] = "Post successfully added!";
+            return View("PostForm", postVM);
         }
     }
 }
