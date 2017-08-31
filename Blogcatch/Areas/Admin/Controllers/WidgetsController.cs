79295296a002b7a9ff7d448ca1b5c41ed6883617
TheEndurance <rawa.jalal@gmail.com>
2017-08-31 18:16:59 -0400

    Create widget selectable/droppable UI and Widget view model

--- Blogcatch/Areas/Admin/Controllers/WidgetsController.cs
+++ Blogcatch/Areas/Admin/Controllers/WidgetsController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Blogcatch.Models;
+using Blogcatch.ViewModel;
 
 namespace Blogcatch.Areas.Admin.Controllers
 {
     public class WidgetsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public WidgetsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Admin/Widgets
         public ActionResult Index()
         {
-            return View();
+            var widgetVM = _context.Widgets.ToArray().Select(x=>new WidgetViewModel(x)).ToList();
+            return View(widgetVM);
         }
     }
 }
---
+++ Blogcatch/ViewModel/WidgetViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Blogcatch.Areas.Admin.Models;
+
+namespace Blogcatch.ViewModel
+{
+    public class WidgetViewModel
+    {
+        public int Id { get; set; }
+
+        
+        public short Sorting { get; set; }
+
+        
+        public bool Enabled { get; set; }
+        
+        public string Type { get; set; }
+
+        public WidgetViewModel(Widget widget)
+        {
+            this.Id = widget.Id;
+            this.Sorting = widget.Sorting;
+            this.Enabled = widget.Enabled;
+            this.Type = widget.Type;
+        }
+    }
+}
