903050f3604eccfdc26f40a73d73888914eaec08
TheEndurance <rawa.jalal@gmail.com>
2017-08-31 21:51:00 -0400

    widget reorder and enable/disable

--- Blogcatch/Areas/Admin/Controllers/WidgetsController.cs
+++ Blogcatch/Areas/Admin/Controllers/WidgetsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using Blogcatch.Models;
 using Blogcatch.ViewModel;
@@ -25,8 +26,34 @@ namespace Blogcatch.Areas.Admin.Controllers
         // GET: Admin/Widgets
         public ActionResult Index()
         {
-            var widgetVM = _context.Widgets.ToArray().Select(x=>new WidgetViewModel(x)).ToList();
+            var widgetVM = _context.Widgets.ToArray().OrderBy(x=>x.Sorting).Select(x=>new WidgetViewModel(x)).ToList();
             return View(widgetVM);
+        }
+
+        // POST: Admin/Widgets/ReorderAndEnableWidgets
+        [System.Web.Http.HttpPost]
+        public ActionResult ReorderAndEnableWidgets(int[] id)
+        {
+            //Disable all widgets
+            var allWidgets = _context.Widgets.ToList();
+            foreach (var widget in allWidgets)
+            {
+                widget.Enabled = false;
+            }
+
+            short count = 0;
+            if (id != null)
+            {
+                foreach (var _id in id)
+                {
+                    var widget = _context.Widgets.Find(_id);
+                    widget.Sorting = count;
+                    widget.Enabled = true;
+                    count++;
+                }
+            }
+            _context.SaveChanges();
+            return Json(new {success = true});
         }
     }
 }
