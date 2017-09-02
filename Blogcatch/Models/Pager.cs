00a99229639bafb613843b75fad4d0b33220c91f
TheEndurance <rawa.jalal@gmail.com>
2017-09-02 19:34:44 -0400

    custom pager class for blog post paging

---
+++ Blogcatch/Models/Pager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blogcatch.Models
+{
+    public class Pager
+    {
+
+        public int TotalItems { get; set; }
+        public int NumItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int StartPage { get; set; }
+        public int EndPage { get; set; }
+
+        public Pager(int totalItems,int? page,int numItemsPerPage = 10,int numPagers = 10)
+        {
+            var totalPages = (int) Math.Ceiling((decimal) totalItems / (decimal) numItemsPerPage);
+            var currentPage = page ?? 1; //if page is null, set the value of current page to 1
+            var startPage = currentPage - (numPagers/2); 
+            var endPage = currentPage + (numPagers/2)-1;
+
+            if (startPage <= 0)
+            {
+                endPage -= (startPage - 1); //add 5 to endPage
+                startPage = 1; //set firstPage to 1
+            }
+
+            if (endPage > totalPages) //If the endPage is more than the number of pages
+            {
+                endPage = totalPages; //set the endPage to the maximum number of pages
+                if (endPage > numPagers) //once the endpage moves past 10 pages  
+                {
+                    startPage = endPage - (numPagers-1);
+                }
+            }
+
+            this.TotalItems = totalItems;
+            this.TotalPages = totalPages;
+            this.NumItemsPerPage = numItemsPerPage;
+            this.CurrentPage = currentPage;
+            this.StartPage = startPage;
+            this.EndPage = endPage;
+        }
+    }
+}
